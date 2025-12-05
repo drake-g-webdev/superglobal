a6d3c0d8be48bcbe588ce9abefbcbc838c3a776b
drake-g-webdev <drake.g.webdev@gmail.com>
2025-12-05 00:01:44 -0500

    Fix /app route SSR build error - disable prerendering for auth-dependent page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,4 +1,11 @@
-import AppContent from '../components/AppContent';
+'use client';
+
+import dynamic from 'next/dynamic';
+
+// Dynamically import AppContent with SSR disabled to avoid AuthProvider issues during build
+const AppContent = dynamic(() => import('../components/AppContent'), {
+  ssr: false,
+});
 
 export default function AppPage() {
   return <AppContent />;
